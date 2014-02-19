cb1286529f45e7197d7b84dbfdc0abda73e1044a
Alex Gaudiosi <aa.gaudiosi@gmail.com>
2014-02-19 18:37:00 +0000

    2nd Exercise

--- directory.rb
+++ directory.rb
@@ -23,8 +23,8 @@ def input_students
 end
 
 def display(students)
-	students.each do |student|
-		print "#{student[:name]} (#{student[:cohort]} cohort) \n" 
+	students.each_with_index do |student, index|
+		print "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort) \n" 
 	end
 end
 
