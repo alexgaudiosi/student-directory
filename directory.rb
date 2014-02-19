317eddf37214e7d61ca45e8b23806ec745db88c1
Alex Gaudiosi <aa.gaudiosi@gmail.com>
2014-02-19 14:27:42 +0000

    Additional information

--- directory.rb
+++ directory.rb
@@ -1,39 +1,39 @@
 # let's put all students into an array
 students = [
-"Mario Gintili",
-"Mikhail Dubov",
-"Karolis Noreika",
-"Michael Sidon",
-"Charles De Barros",
-"Ruslan Vikhor",
-"Toby Retallick",
-"Mark Mekhaiel",
-"Sarah Young",
-"Hannah Wight",
-"Khushkaran Singh",
-"Rick Brunstedt",
-"Manjit Singh",
-"Alex Gaudiosi",
-"Ross Hepburn",
-"Natascia Marchese",
-"Tiffanie Chia",
-"Matthew Thomas",
-"Freddy McGroarty",
-"Tyler Rollins",
-"Richard Curteis",
-"Anna Yanova",
-"Andrew Cumine"
+["Mario Gintili", :february],
+["Mikhail Dubov", :february],
+["Karolis Noreika", :february],
+["Michael Sidon", :february],
+["Charles De Barros", :february],
+["Ruslan Vikhor", :february],
+["Toby Retallick", :february],
+["Mark Mekhaiel", :february],
+["Sarah Young", :february],
+["Hannah Wight", :february],
+["Khushkaran Singh", :february],
+["Rick Brunstedt", :february],
+["Manjit Singh", :february],
+["Alex Gaudiosi", :february],
+["Ross Hepburn", :february],
+["Natascia Marchese", :february],
+["Tiffanie Chia", :february],
+["Matthew Thomas", :february],
+["Freddy McGroarty", :february],
+["Tyler Rollins", :february],
+["Richard Curteis", :february],
+["Anna Yanova", :february],
+["Andrew Cumine", :february]
 ]
+
+def print(students)
+	students.each do |student|
+		puts "#{student[0]} (#{student[1]} cohort)"
+	end
+end
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
 	puts "-----------"
-end
-
-def print(names)
-	names.each do |name|
-		puts name
-	end
 end
 
 def print_footer(names)
